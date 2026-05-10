a17d70ba24c588ce691c595d99dee94a3aecf031
leventefo <fo.levente@gmail.com>
2026-05-11 00:01:24 +0200

    Testing

--- streamlit_app.py
+++ streamlit_app.py
@@ -148,7 +148,8 @@ def main():
 
     st.write(datetime.now() - last_cleanup_time >= timedelta(seconds=10))
 
-    st.write(datetime.now())
+    st.write("server time", datetime.now())
+    st.write("file time", last_cleanup_time)
 
     if datetime.now() - last_cleanup_time >= timedelta(seconds=10):
 
